fe1f13f8cf35bd668896d8e29f505f2addcf3751
ayush <71914381+Ayush-08-beep@users.noreply.github.com>
2023-10-13 16:19:28 +0530

    Create updates.js

---
+++ js/updates.js
@@ -0,0 +1,8 @@
+<div class="slideshow">
+  <a href="link1.html"><img src="image1.jpg"></a>
+  <a href="link2.html"><img src="image2.jpg"></a>
+  <a href="link3.html"><img src="image3.jpg"></a>
+</div>
+
+<button id="prev">Prev</button>
+<button id="next">Next</button>
